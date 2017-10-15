5426791468aa9a3ed8e9b9f4c3448e3ba5717954
Nick Foden <nickfoden@gmail.com>
2017-10-15 14:17:12 -0400

    Refactored to basic app.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,46 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import YTSearch from 'youtube-api-search';
+import SearchBar from'./components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+const API_KEY = 'AIzaSyBBF51Ccf2N8WCSRTceuBb8lpUlsaFrGI8';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+class App extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
+        this.videoSearch('Joe Rogan');
+    }
+
+    videoSearch(term){
+        YTSearch({key: API_KEY, term: term + 'Joe Rogan Experience'}, (videos) => {
+            this.setState({ 
+                videos: videos,
+                selectedVideo: videos[0]
+            });
+        });
+    }
+
+    render() {
+        const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 400)
+
+        return (
+            <div>
+                <SearchBar onSearchTermChange={videoSearch}/>
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList 
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos} />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
