4a7063dcb0eb4dd58370dd34c3d4c72b8cb0a9d4
Nick Foden <nickfoden@gmail.com>
2018-02-01 21:24:45 -0500

    Moved api key to dot env and updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,32 @@
 module.exports = {
-    entry: [
-      './src/index.js'
-    ],
-    output: {
-      path: __dirname,
-      publicPath: '/',
-      filename: 'bundle.js'
-    },
-    module: {
-      loaders: [{
+  entry: ["./src/index.js"],
+  output: {
+    path: __dirname,
+    publicPath: "/",
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
         exclude: /node_modules/,
-        loader: 'babel',
+        loader: "babel",
         query: {
-          presets: ['react', 'es2015', 'stage-1']
+          presets: ["react", "es2015", "stage-1"]
         }
-      }]
-    },
-    resolve: {
-      extensions: ['', '.js', '.jsx']
-    },
-    devServer: {
-      historyApiFallback: true,
-      contentBase: './',
-      host: '0.0.0.0',
-      port: '3000',
-      disableHostCheck: true
-    }
-  };
-  
+      }
+    ]
+  },
+  resolve: {
+    extensions: ["", ".js", ".jsx"]
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: "./",
+    host: "0.0.0.0",
+    port: "3000",
+    disableHostCheck: true
+  },
+  node: {
+    fs: "empty"
+  }
+};
