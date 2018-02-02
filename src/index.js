9c3845c8fed69f5686a41e68962bd058674b3f58
Nick Foden <nickfoden@gmail.com>
2018-02-01 22:08:35 -0500

    API Key moved to env

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import YTSearch from "youtube-api-search";
 import SearchBar from "./components/search_bar";
 import VideoList from "./components/video_list";
 import VideoDetail from "./components/video_detail";
-const { API_KEY } = require("../config");
+const API_KEY = require("../config");
 
 class App extends Component {
   constructor(props) {
